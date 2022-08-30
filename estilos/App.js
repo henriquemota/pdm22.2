a3b4b2d0ab1185f97c8065f5bffa72e0d87f7a01
henriquemota <mota.henrique@gmail.com>
2022-08-30 09:31:42 -0300

    implementacao do layout com flexbox

--- estilos/App.js
+++ estilos/App.js
@@ -31,10 +31,18 @@ const STYLES = StyleSheet.create({
 
 export default function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor:
+          'rgba(200,0,200,0.6)',
+        justifyContent: 'center',
+      }}
+    >
       <View style={[STYLES.quadrado01, STYLES.bgVermelho]} />
-      <View style={[STYLES.quadrado02, STYLES.bgVerde]} />
-      <View style={[STYLES.quadrado03, STYLES.bgAzul, STYLES.round]} />
+      <View style={[STYLES.quadrado01, STYLES.bgVerde]} />
+      <View style={[STYLES.quadrado01, STYLES.bgAzul]} />
       <Image
         source={{ uri: 'https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg' }}
         style={[STYLES.quadrado03, STYLES.bgAzul, STYLES.round]}
