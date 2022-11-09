5c8c2574b7df257c72a2e8ed83f66d3760ac2bf2
henriquemota <mota.henrique@gmail.com>
2022-11-09 20:01:54 -0300

    atualizando no github

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -36,27 +36,52 @@ function useDatabase() {
     // const db = SQLite.openDatabase("database.db")
     const db = DatabaseConnection.getConnection()
     db.exec(
-      [{ sql: 'insert into todos (nome) values (?);', args: [tarefa] }],
+      [
+        { sql: 'insert into todos (conteudo) values (?);', args: [tarefa] },
+        { sql: 'insert into todos (conteudo) values (?);', args: [tarefa] },
+        { sql: 'insert into todos (conteudo) values (?);', args: [tarefa] },
+        { sql: 'insert into todos (conteudo) values (?);', args: [tarefa] },
+        { sql: 'insert into todos (conteudo) values (?);', args: [tarefa] },
+        { sql: 'insert into todos (conteudo) values (?);', args: [tarefa] },
+      ],
       false,
       () => console.log('Linhas inseridas')
     )
   }
-  function select() {
-    const db = DatabaseConnection.getConnection()
-    db.exec(
-      [{ sql: 'select * from todos;', args: [] }],
-      true,
-      (e) => {
-        console.log('select funcionou')
-        console.log(e)
-      }
-    )
+  function findById(id) {
+    return new Promise(function (resolve, reject) {
+      const db = DatabaseConnection.getConnection()
+      db.transaction(tx => {
+        tx.executeSql(`select * from todos where id=?`, [id], (_, { rows }) => {
+          resolve(rows)
+        }), (sqlError) => {
+          reject(sqlError)
+        }
+      }, (txError) => {
+        reject(txError)
+      })
+    })
+  }
+  function findAll() {
+    return new Promise(function (resolve, reject) {
+      const db = DatabaseConnection.getConnection()
+      db.transaction(tx => {
+        tx.executeSql(`select * from todos;`, [], (_, { rows }) => {
+          resolve(rows)
+        }), (sqlError) => {
+          reject(sqlError)
+        }
+      }, (txError) => {
+        reject(txError)
+      })
+    })
   }
 
   return {
     initDb,
     insert,
-    select,
+    findById,
+    findAll,
   }
 }
 
--- api/src/pages/SQLite.jsx
+++ api/src/pages/SQLite.jsx
@@ -1,32 +1,63 @@
-import { View } from 'react-native'
-import { Button } from 'react-native-paper'
+import { useEffect, useState } from 'react'
+import { FlatList, View } from 'react-native'
+import { Button, Text, TextInput } from 'react-native-paper'
 import useDatabase from '../database'
 
 function SQLite() {
-  const { initDb, select, insert } = useDatabase()
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState('')
+  const { initDb, findAll, insert } = useDatabase()
+
+  useEffect(() => {
+    search()
+  }, [])
+
+  function search() {
+    findAll().then(({ _array }) => setTodos(_array))
+  }
+
+  function save() {
+    insert(todo)
+    setTodo('')
+    search()
+  }
 
   return (<View style={{ padding: 16 }}>
     <Button
       mode='contained'
       style={{ marginTop: 16 }}
-      onPress={initDb}
+      onPress={function () {
+        initDb()
+        search()
+      }}
     >
       Inicializar DB
     </Button>
     <Button
       mode='contained'
       style={{ marginTop: 16 }}
-      onPress={select}
+      onPress={findAll}
     >
       Selecionar registros
     </Button>
+    <TextInput
+      placeholder='informe a atividade'
+      onChangeText={text => setTodo(text)}
+      value={todo}
+    />
     <Button
       mode='contained'
       style={{ marginTop: 16 }}
-      onPress={() => insert('nova tarefa')}
+      onPress={save}
     >
       Inserir registro
     </Button>
+    <FlatList
+      data={todos}
+      renderItem={({ item }) => <Text style={{ margin: 16, fontSize: 32 }}>{item.conteudo}</Text>}
+      keyExtractor={item => item.id}
+    />
+
   </View>)
 }
 
